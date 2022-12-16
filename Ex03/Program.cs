193760a69cb89925ce1ee21ffa0dca7d88978c69
Sergey <glad-nevers@ya.ru>
2022-12-16 14:39:19 +0700

    Сделал 3 задачу, как делать дальше не знаю

---
+++ Ex03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов (из диапазона [0-14] ) и выводит на экран десятичное представление числа, записанного в СС по основанию 15
+
+// N: 3 [9, 4, 12] => 2097
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 14);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
